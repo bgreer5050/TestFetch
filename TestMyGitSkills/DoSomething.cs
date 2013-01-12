a7a8c017941004f262e3ee197dacc966b8e0077d
bgreer <bgreer5050@gmail.com>
2013-01-12 17:22:22 -0600

    Initial

---
+++ TestMyGitSkills/DoSomething.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestMyGitSkills
+{
+    public class DoSomething
+    {
+        public static void Write()
+        {
+
+            Console.WriteLine("We wrote from DoSomething.Write");
+            Console.WriteLine("We wrote from DoSomething.Write2");
+
+
+        }
+    }
+}
---
+++ TestMyGitSkills/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestMyGitSkills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+
+            DoSomething.Write();
+
+
+            Console.ReadLine();
+
+        }
+
+
+       
+
+    }
+}
